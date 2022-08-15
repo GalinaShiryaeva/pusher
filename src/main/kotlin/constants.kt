bac04f4ca4393e73d550a5999c49dc342a0d650c
GalinaShiryaeva <Galla1777.35@gmail.com>
2022-08-15 15:16:42 +0300

    pusher Initial commit

---
+++ src/main/kotlin/constants.kt
@@ -0,0 +1,4 @@
+package ru.netology.pusher
+
+//const val dbUrl = "firebase-adminsdk-l995n@nmedia-2875c.iam.gserviceaccount.com"
+const val token = "e_wKQzFQQdm86tX6HFe-7P:APA91bE7jsoppBspEpnHIGG932txRpzAmHEUnVmB8TKC2XoJCp9-tgIgzUKUoz8w_2tGFG-WoVUk-PpTEYq5QmvUE1Jv8gfZWGA7kwj_WfeBM28L1n0gtsdUGRFZIl7WT-wetqdM6Ell"
